85101b3b8eaa4493ce48eafb5dfa94750edcc876
nprabhu <nprabhu26@gmail.com>
2013-03-07 01:36:01 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drewtech::Application.config.secret_token = '0311393ddb32489b8591c8a05386b01a9cce4a2f8a33d410123eb3c203bad4a25cc1515aaec607e94624c462bcaa9780f5cdaa4167db9eadbb7499f2ac243534'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Drewtech::Application.config.session_store :cookie_store, key: '_drewtech_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Drewtech::Application.config.session_store :active_record_store
